65ade09f43c38b1a3bfe9e5ff98470285cf9ee98
Nicolas Pintaux <npintaux@pivotal.io>
2019-01-25 15:47:37 +0100

    Fixed script to create terraform file.

---
+++ scripts/init-opsman.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source "${HOME}/.env"
+echo "Initializing Pivotal Cloud Foundry Operations Manager ${OPSMAN_VERSION}"
+
+
+PRODUCT_NAME="Pivotal Application Service (formerly Elastic Runtime)" \
+DOWNLOAD_REGEX="Azure Terraform Templates" \
+PRODUCT_VERSION=${PAS_VERSION} \
+  ${HOME}/scripts/download-product.sh || exit 1
+unzip -o ${HOME}/downloads/elastic-runtime_${PAS_VERSION}_*/terraforming-azure-*.zip -d ${HOME}
+
